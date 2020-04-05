9abc373020e0f540e075cd4e184dd09387357ba3
Charlie <charliebullock8@gmail.com>
2020-04-05 13:47:33 +0100

    Adding static page controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  #protect_from_forgery with: :exception
+
 def hello
-  render text: "Hello, world!"
+  render plain: "Hi charlie"
 end
 
 end
