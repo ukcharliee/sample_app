c576facde09b907a141cd2d526318098ffa341cb
Charlie <charliebullock8@gmail.com>
2020-04-21 14:52:10 +0100

    Finish user sign up

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,7 @@ Rails.application.routes.draw do
 #Users
   get '/sign_up', to: 'users#new'
 
+  resources :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
